04947a37dc47c4d849ad6def9dd7594aa73da19f
future.wei <future.wei@technet-asia.com>
2019-08-22 15:06:41 +0800

    update

---
+++ src/main/java/com/example/javamavenjunithelloworld/Demo1Test.java
@@ -0,0 +1,16 @@
+package com.example.javamavenjunithelloworld;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class Demo1Test   
+{  
+    @Test  
+    public void TestMth1() {  
+        assertTrue(1==1);  
+    }  
+      
+    @Test  
+    public void TestMth2() {  
+        assertTrue(1==1);  
+    }  
+}  
